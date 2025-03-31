465747c653bef33d563ec4b15271276d994f7a56
Egurnova Elena <gamedev409@yandex.ru>
2025-03-31 13:29:13 +0500

    Добавьте файлы проекта.

---
+++ BooksLibrary/ViewModels/MainViewModel.cs
@@ -0,0 +1,67 @@
+﻿using BooksLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksLibrary.ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private string _title;
+        private string _author;
+        private int _year;
+        private Book _selectedBook;
+
+        public ObservableCollection<Book> Books { get; } = new();
+
+        public string Title
+        {
+            get { return _title; }
+            set 
+            { 
+                _title = value; 
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+
+        public string Author
+        {
+            get { return _author; }
+            set
+            {
+                _author = value;
+                OnPropertyChanged(nameof(Author));
+            }
+        }
+
+        public int Year
+        {
+            get { return _year; }
+            set
+            {
+                _year = value;
+                OnPropertyChanged(nameof(Year));
+            }
+        }
+        public Book SelectedBook
+        {
+            get { return _selectedBook; }
+            set
+            {
+                _selectedBook = value;
+                OnPropertyChanged(nameof(SelectedBook));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
